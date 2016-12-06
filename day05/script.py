97b653c3c29508be0c37da5514d46a222b688b47
Andreas Reich <r_andreas2@web.de>
2016-12-06 09:38:58 +0000

    solution for day 5 part 2

--- day05/script.py
+++ day05/script.py
@@ -5,12 +5,25 @@ from hashlib import md5
 doorId = 'uqwqemis'
 
 PASS_LEN = 8
-password = ''
+passwordP1 = ''
+passwordP2 = ['_'] * PASS_LEN
 
 i = 0
-while len(password) != PASS_LEN:
+while any([c == '_' for c in passwordP2]):
     md5string = md5((doorId +str(i)).encode()).hexdigest()
     i += 1
     if md5string.startswith('00000'):
-        password += md5string[5]
-        print(password)
+        if len(passwordP1) != PASS_LEN:
+            passwordP1 += md5string[5]
+
+        try:
+            pos = int(md5string[5])
+        except:
+            continue
+        if pos < PASS_LEN and passwordP2[pos] == '_':
+            passwordP2[pos] = md5string[6]
+            print(passwordP2)
+
+
+print("part one:", passwordP1)
+print("part two:", ''.join(passwordP2))
